952a88baf6f99641de1c6ffb1d205a8a013924e5
Guilherme <gemdias@hotmail.com>
2020-07-08 22:20:05 -0300

    Initial commit with sandbox project.

---
+++ src/main/java/com/guilherme/sandbox/business1/controller/Controller1.java
@@ -0,0 +1,25 @@
+package com.guilherme.sandbox.business1.controller;
+
+import com.guilherme.sandbox.business1.model.Model1;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/example"})
+public class Controller1 {
+
+    @GetMapping
+    @RequestMapping(value = "/getModel1", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Model1> businessRule(){
+
+        //TODO: Create Facade - Service - Client Pattern
+        Model1 model1 = new Model1();
+        model1.setId((long) 1);
+        model1.setName("Hello World");
+
+        return ResponseEntity.ok().body(model1);
+    }
+}
---
+++ src/main/java/com/guilherme/sandbox/business1/model/Model1.java
@@ -0,0 +1,22 @@
+package com.guilherme.sandbox.business1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Model1 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("model_name")
+    private String name;
+}
